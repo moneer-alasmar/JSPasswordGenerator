2668207625872f4fe94408c46d9b2d7382a7a2f1
Moneer <kiraalasmar@gmail.com>
2019-10-29 17:03:13 -0700

    began password generation logic

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+// DOM Elements
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numberEl = document.getElementById("numbers");
+const symbolEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunc = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// Generate Event Listeners
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasUpper = uppercaseEl.checked;
+  const hasLower = lowercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Generate Password Function
+
 // Generator Functions - http://www.net-comber.com/charset.html
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
